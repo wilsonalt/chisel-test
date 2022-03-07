fc1a872d2c27da05a2305468a48cc461c6d37d52
Daniel Ferragut <danielferragut@profusion.mobi>
2022-03-07 18:21:20 -0300

    feat: add hello endpoint

---
+++ endpoints/hello.ts
@@ -0,0 +1,18 @@
+import { responseFromJson } from "@chiselstrike/api"
+import { BlogComment } from "../models/BlogComment"
+
+type Handler = (req: Request, res: Response) => Response | Promise<Response>
+
+const handleGet: Handler = async req => {
+  return responseFromJson({hello: "world"})
+}
+
+const handlers: Record<string, Handler> = {
+  GET: handleGet,
+}
+
+export default async function chisel(req: Request, res: Response) {
+  if (handlers[req.method] === undefined)
+    return new Response(`Unsupported method ${req.method}`, { status: 405 })
+  return handlers[req.method](req, res)
+}
