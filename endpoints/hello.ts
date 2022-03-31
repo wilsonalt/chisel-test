126e607f1690c6c71d3ae937dd4945730a8a25f9
Daniel Pereira Ferragut <danielferragut@profusion.mobi>
2022-03-31 16:12:42 -0300

    Update hello.ts

--- endpoints/hello.ts
+++ endpoints/hello.ts
@@ -5,7 +5,7 @@ type Handler = (req: Request, res: Response) => Response | Promise<Response>
 
 const handleGet: Handler = async req => {
 // hello
-  return responseFromJson({hello: "world github org 3"})
+  return responseFromJson({hello: "world github org 3 new chisel"})
 }
 
 const handlers: Record<string, Handler> = {
